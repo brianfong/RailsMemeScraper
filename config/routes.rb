c0c2b46fe19c8bb822fdaf1b4f32c6bd52f7d078
brianfong <brianjonathanfong@gmail.com>
2019-04-30 13:37:34 -0700

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get 'carousel/new'
   # get 'carousel/create'
 
-  resources :memes, only: [:index]
+  #resources :memes, only: [:index]
+  root to: 'memes#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
